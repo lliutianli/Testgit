5774ac066a95b47a68703f491af4d2ca71099fa9
liutao_pro <tao.liu11@hand-china.com>
2019-06-04 20:56:45 +0800

    test

--- src/main/java/com/hand/git/Test.java
+++ src/main/java/com/hand/git/Test.java
@@ -2,13 +2,7 @@ package com.hand.git;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("right...");
-		System.out.println("left ...");
-
-		System.out.println("error...");
-
-		System.out.println("T1000...");
-
+		System.out.println("Testgit...");
 	}
 
 }
